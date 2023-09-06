b80daefcbb9b97c86e300fd98ccc49b585cb9dc4
guirab <guilhermesr2000@gmail.com>
2023-09-06 00:32:11 -0300

    feat: texto mudando de cor com o mouse

--- components/mouse-over.tsx
+++ components/mouse-over.tsx
@@ -1,52 +1,55 @@
-/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { useEffect } from "react";
 
 export const MouseOver = () => {
   useEffect(() => {
-    document.addEventListener("mousemove", (e) => {
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
+    const glass = document.querySelector(".magnifying-glass") as HTMLElement;
+    const container = document.querySelector(".container");
+    if (!container) return console.log("no container");
 
-      const anchor = document.getElementById("anchor");
-      const rekt = anchor?.getBoundingClientRect();
-      if (!rekt) return console.log("no rekt");
-      const anchorX = rekt?.left + rekt?.width / 2;
-      const anchorY = rekt?.right + rekt?.height / 2;
+    const cInfo = container.getBoundingClientRect();
+    if (!glass) return console.log("no glass");
 
-      const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
-      const rotate = document.getElementById("rotate");
-      if (!rotate) return console.log("no rotate");
-      rotate.style.transform = `rotate(${angleDeg}deg)`;
-      if (!anchor) return console.log("no anchor");
-      anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+    document.addEventListener("pointermove", (e) => {
+      glass.style.opacity = "1";
+      glass.style.transform = `translate(
+            ${e.clientX - cInfo.x - glass.getBoundingClientRect().width / 2}px, 
+            ${e.clientY - cInfo.y - glass.getBoundingClientRect().height / 2}px
+        )`;
+      const h1 = glass.querySelector("h1");
+      if (!h1) return console.log("no h1");
+
+      h1.style.transform = `translate(
+            ${
+              (e.clientX - glass.getBoundingClientRect().width / 2 - cInfo.x) *
+              -1
+            }px,
+            ${
+              (e.clientY - glass.getBoundingClientRect().height / 2 - cInfo.y) *
+              -1
+            }px)`;
     });
   }, []);
 
-  const angle = (cx: number, cy: number, ex: number, ey: number) => {
-    const dy = ey - cy;
-    const dx = ex - cx;
-    const theta = Math.atan2(dy, dx);
-    const deg = (theta * 180) / Math.PI;
-    return deg;
-  };
-
   return (
-    <div id="anchor" className="flex justify-center items-center flex-col px-8">
+    <div className="flex justify-center items-center flex-col px-8">
       <h1 className="text-4xl font-bold">About me</h1>
-      <br />
-      <button
-        id="rotate"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Download CV
-      </button>
-      <p className="text-2xl font-semibold text-left">
-        I am a solution-oriented web developer with an eye for detail and
-        effectiveness. I have 3-4 years of experience in programming web
-        applications and I am eager to learn more about this field and becoming
-        a better professional.
-      </p>
+      <div className="container">
+        <h1 className="display">
+          I am a solution-oriented web developer with an eye for detail and
+          effectiveness. I have 3-4 years of experience in programming web
+          applications and I am eager to learn more about this field and
+          becoming a better professional.
+        </h1>
+        <div className="magnifying-glass">
+          <h1 className="magnify-fx">
+            I am a solution-oriented web developer with an eye for detail and
+            effectiveness. I have 3-4 years of experience in programming web
+            applications and I am eager to learn more about this field and
+            becoming a better professional.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
